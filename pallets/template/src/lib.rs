9cd2d1565d3b4c8bef43ed0f2daefe7521d8a93f
Sang Dang Dinh <sang.dang.connectiv@gmail.com>
2022-07-04 22:50:52 +0700

    Initial commit

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -16,7 +16,7 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::pallet_prelude::*;
+	use frame_support::{pallet_prelude::{*, ValueQuery}, Blake2_128Concat};
 	use frame_system::pallet_prelude::*;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
@@ -37,6 +37,12 @@ pub mod pallet {
 	// Learn more about declaring storage items:
 	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
+
+	#[pallet::storage]
+	pub type Number<T:Config> = StorageMap<_,Blake2_128Concat,
+		            T::AccountId,
+					u32,
+					ValueQuery, >;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
@@ -77,6 +83,14 @@ pub mod pallet {
 			// Emit an event.
 			Self::deposit_event(Event::SomethingStored(something, who));
 			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1, 1))]
+		pub fn put_number(origin: OriginFor<T>, number: u32) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			<Number<T>>::insert(who.clone(), number);
+			Self::deposit_event(Event::SomethingStored(number, who));
 			Ok(())
 		}
 
